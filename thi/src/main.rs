3dcd5b9231d1dc87c943608629ab16188b3fed0a
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-06-14 15:38:10 +0900

    yeah

--- thi/src/main.rs
+++ thi/src/main.rs
@@ -1,6 +1,7 @@
 #![no_main]
 #![no_std]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,21 +9,28 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(),
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+    let dat = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8];
+    let trigger = [0xACu8, 0x33u8, 0x00u8];
+    arduino_hal::delay_ms(1000);
 
-    let mut led = pins.d13.into_output();
+    ufmt::uwriteln!(&mut serial, "Write direction test:\r").void_unwrap();
+    i2c.i2cdetect(
+        &mut serial,
+        arduino_hal::i2c::I2cOps::raw_write(&mut serial, 0xACu8),
+    );
+    ufmt::uwriteln!(&mut serial, "\r\nRead direction test\r").void_unwrap();
+    i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Read);
+    // let mut led = pins.d13.into_output();
 
     loop {
-        led.toggle();
         arduino_hal::delay_ms(1000);
     }
 }
