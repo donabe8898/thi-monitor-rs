93f7a60c950c634be7a33bf3c64b0c0eec773c3c
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-06-21 11:00:01 +0900

    uuu

--- thi/src/main.rs
+++ thi/src/main.rs
@@ -1,8 +1,30 @@
 #![no_main]
 #![no_std]
 
-use arduino_hal::prelude::*;
+use core::fmt::Error;
+
+use arduino_hal::{prelude::*, i2c::Direction};
 use panic_halt as _;
+
+// use i2c sensor address
+// Write direction test:
+//
+// -    0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+// 00:       -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 30: -- -- -- -- -- -- -- -- 38 -- -- -- -- -- -- --
+// 40: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 50: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 60: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 70: -- -- -- -- -- -- -- --
+
+// Read direction test
+// -    0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+// 00:       -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 10: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// 30: -- -- -- -- -- -- -- -- 38^C
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -17,20 +39,31 @@ fn main() -> ! {
         pins.a5.into_pull_up_input(),
         50000,
     );
-    let dat = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8];
-    let trigger = [0xACu8, 0x33u8, 0x00u8];
-    arduino_hal::delay_ms(1000);
+    let mut dat = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8];
+    let mut trigger = [0xACu8, 0x33u8, 0x00u8];
 
     ufmt::uwriteln!(&mut serial, "Write direction test:\r").void_unwrap();
-    i2c.i2cdetect(
-        &mut serial,
-        arduino_hal::i2c::I2cOps::raw_write(&mut serial, 0xACu8),
-    );
+    i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Write);
     ufmt::uwriteln!(&mut serial, "\r\nRead direction test\r").void_unwrap();
     i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Read);
-    // let mut led = pins.d13.into_output();
+    let mut led = pins.d13.into_output();
+
 
     loop {
+        arduino_hal::delay_ms(100);
+        i2c.write(0x38u8,&mut trigger);
+
+        arduino_hal::delay_ms(80);
+        i2c.read(0x38,&mut dat);
+
+        let hum: u32 = (dat[1] as u32) << 12 | (dat[2] as u32) << 4 | (((dat[3] as u32) & 0xF0) >> 4);
+        let tmp: u32 =  ((dat[3] as u32 & 0x0F) << 16) | (dat[4] as u32 )<< 8 | (dat[5] as u32);
+
+        let calced_hum = hum as u32 / 1048576 * 100;
+        let calced_tmp = tmp as u32 / 1048576 * 200-50;
+
+        ufmt::uwriteln!(&mut serial,"{}",calced_tmp).void_unwrap();
         arduino_hal::delay_ms(1000);
     }
 }
+
