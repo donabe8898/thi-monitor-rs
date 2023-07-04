350d87398f0bbae48f5eb165ff9fb68a70a0b193
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-07-04 16:51:13 +0900

    LED OK

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_main]
 #![no_std]
 
-use arduino_hal::prelude::*;
+use arduino_hal::{port::mode::Output, prelude::*};
 use panic_halt as _;
 use ufmt::uwriteln;
 use ufmt_float::uFmt_f32;
@@ -31,6 +31,10 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
+    const LED_PIN_RED: u8 = 9;
+    const LED_PIN_GRN: u8 = 10;
+    const LED_PIN_BLE: u8 = 11;
+
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     let mut i2c = arduino_hal::I2c::new(
@@ -40,16 +44,20 @@ fn main() -> ! {
         50000,
     );
 
+    let mut red_led = pins.d9.into_output();
+    let mut grn_led = pins.d10.into_output();
+    let mut ble_led = pins.d11.into_output();
+
     let mut check = [0x71u8];
     let mut dat = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8];
     let mut trigger = [0xACu8, 0x33u8, 0x00u8];
 
-    let mut _led = pins.d13.into_output();
-
     // 初期チェック
     arduino_hal::delay_ms(100);
     let _ret: Result<(), arduino_hal::i2c::Error> = i2c.read(0x38, &mut check);
-
+    red_led.set_high();
+    ble_led.set_high();
+    grn_led.set_high();
     loop {
         // データ書き込み
         arduino_hal::delay_ms(100);
@@ -68,18 +76,24 @@ fn main() -> ! {
         // 温度と湿度に変換
         let calced_tmp = (tmp as f32 / 1048576.0) * 200.0 - 50.0;
         let calced_hum = (hum as f32 / 1048576.0) * 100.0;
+        // 不快指数計算
+        let calced_thi = 0.81 * calced_tmp + 0.01 * calced_hum * (0.99 * calced_tmp - 14.3) + 46.3;
 
         // フォーマット
         let fmted_tmp = uFmt_f32::Zero(calced_tmp);
         let fmted_hum = uFmt_f32::Zero(calced_hum);
+        let fmted_thi = uFmt_f32::Zero(calced_thi);
 
-        // error
+        // 一旦表示
         uwriteln!(&mut serial, "===================").void_unwrap();
         uwriteln!(&mut serial, "室温 {}℃", fmted_tmp).void_unwrap();
         uwriteln!(&mut serial, "湿度 {}%", fmted_hum).void_unwrap();
+        uwriteln!(&mut serial, "不快指数 {}", fmted_thi).void_unwrap();
         uwriteln!(&mut serial, "===================").void_unwrap();
 
-        arduino_hal::delay_ms(1000);
+        arduino_hal::delay_ms(50);
+        red_led.toggle();
+        ble_led.toggle();
     }
 }
 
