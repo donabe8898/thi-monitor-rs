bae397f2e646df2ac3d221113313ab2f162741d8
donabe8898 <60559703+donabe8898@users.noreply.github.com>
2023-07-05 23:04:41 +0900

    抽象化したから注視しようか
    Signed-off-by: donabe8898 <60559703+donabe8898@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![no_main]
 #![no_std]
 
-use arduino_hal::{port::mode::Output, prelude::*};
+use arduino_hal::prelude::*;
 use panic_halt as _;
 use ufmt::uwriteln;
 use ufmt_float::uFmt_f32;
@@ -26,17 +26,36 @@ use ufmt_float::uFmt_f32;
 // 20: -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 // 30: -- -- -- -- -- -- -- -- 38^C
 
+trait T {
+    fn calc_thi(&self) -> u32 {
+        1
+    }
+    fn set(&mut self, _input_tmp: f32, _input_hum: f32) {}
+}
+struct FloatData {
+    tmp: f32,
+    hum: f32,
+}
+
+impl T for FloatData {
+    fn calc_thi(&self) -> u32 {
+        (0.81 * self.tmp + 0.01 * self.hum * (0.99 * self.tmp - 14.3) + 46.3) as u32
+    }
+    fn set(&mut self, input_tmp: f32, input_hum: f32) {
+        self.tmp = input_tmp;
+        self.hum = input_hum;
+    }
+}
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    const LED_PIN_RED: u8 = 9;
-    const LED_PIN_GRN: u8 = 10;
-    const LED_PIN_BLE: u8 = 11;
-
+    // シリアルコンソール
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
+    // 温湿度センサ用のi2cオブジェクト
     let mut i2c = arduino_hal::I2c::new(
         dp.TWI,
         pins.a4.into_pull_up_input(),
@@ -44,12 +63,16 @@ fn main() -> ! {
         50000,
     );
 
+    // RGB LED用のオブジェクト
     let mut red_led = pins.d9.into_output();
     let mut grn_led = pins.d10.into_output();
     let mut ble_led = pins.d11.into_output();
 
+    // チェック信号
     let mut check = [0x71u8];
+    // 受信用バッファ
     let mut dat = [0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8, 0x00u8];
+    // セットトリガー
     let mut trigger = [0xACu8, 0x33u8, 0x00u8];
 
     // 初期チェック
@@ -58,6 +81,10 @@ fn main() -> ! {
     red_led.set_low();
     ble_led.set_low();
     grn_led.set_low();
+
+    // コンストラクト
+    let mut data: FloatData = FloatData { tmp: 0.0, hum: 0.0 };
+
     loop {
         // データ書き込み
         arduino_hal::delay_ms(100);
@@ -74,21 +101,24 @@ fn main() -> ! {
             (((dat[3] as u32 & 0x0F) << 16) | (dat[4] as u32) << 8u8 | dat[5] as u32).into();
 
         // 温度と湿度に変換
-        let calced_tmp = (tmp as f32 / 1048576.0) * 200.0 - 50.0;
-        let calced_hum = (hum as f32 / 1048576.0) * 100.0;
+
+        data.set(
+            (tmp as f32 / 1048576.0) * 200.0 - 50.0,
+            (hum as f32 / 1048576.0) * 100.0,
+        );
         // 不快指数計算
-        let calced_thi = 0.81 * calced_tmp + 0.01 * calced_hum * (0.99 * calced_tmp - 14.3) + 46.3;
-        let calced_thi = calced_thi as u32;
+        let thi = data.calc_thi();
+
         // フォーマット
-        let fmted_tmp = uFmt_f32::Zero(calced_tmp);
-        let fmted_hum = uFmt_f32::Zero(calced_hum);
-        // let fmted_thi = uFmt_f32::Zero(calced_thi);
+        let fmted_tmp = uFmt_f32::Zero(data.tmp);
+        let fmted_hum = uFmt_f32::Zero(data.hum);
+        // let fmted_thi = uFmt_f32::Zero(thi as f32);
 
         // 一旦表示
         uwriteln!(&mut serial, "===================").void_unwrap();
         uwriteln!(&mut serial, "室温 {}℃", fmted_tmp).void_unwrap();
         uwriteln!(&mut serial, "湿度 {}%", fmted_hum).void_unwrap();
-        uwriteln!(&mut serial, "不快指数 {}", calced_thi).void_unwrap();
+        uwriteln!(&mut serial, "不快指数 {}", thi).void_unwrap();
         uwriteln!(&mut serial, "===================").void_unwrap();
 
         // ~50 青点滅
@@ -97,11 +127,11 @@ fn main() -> ! {
         // 60~65 白
         // 65~70 緑 快適
         // 70~75 黄色
-        // 75~80 オレンジ
+        // 75~80 黄色点滅
         // 80~85　赤
         // 85~ 赤点滅
 
-        match calced_thi {
+        match thi {
             0...49 => {
                 if red_led.is_set_high() {
                     red_led.set_low();
